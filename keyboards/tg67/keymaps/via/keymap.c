3abde1a8419fa8071e056c19c330a1ea56525070
Ahmed Alhibshi <aralhibshi@gmail.com>
2024-02-19 01:50:54 +0300

    TG67: Default Lighting

--- keyboards/tg67/keymaps/via/keymap.c
+++ keyboards/tg67/keymaps/via/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2]=LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, MO(3),
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_P4, KC_PGUP,
         KC_ENT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_P2,  KC_PGUP,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_RSFT, KC_RALT,  KC_SLSH, KC_P3,          KC_UP,   KC_TAB,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_LEFT,   KC_P1,          KC_LEFT, KC_DOWN, KC_RGHT),
